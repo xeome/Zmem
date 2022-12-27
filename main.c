ee04dd2f8456291b49e1faa364c1d37462f49659
xeome <xeome@proton.me>
2022-12-28 00:40:45 +0300

    refactor further

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <memory.h>
 
 #define HIST_SIZE 50  // Number of values in the history
 #define DELAY_MS 200  // Delay between each update in milliseconds
@@ -69,61 +70,51 @@ int main() {
 void draw_line_graph(const double *history, int size, int graph_height) {
     double max = 0;
     double min = 0;
+
+    // Find the maximum and minimum values in the history array
     for (int i = 0; i < size; ++i) {
-        if (history[i] > max) {
-            max = history[i];
-        }
-        if (history[i] < min) {
-            min = history[i];
-        }
+        max = (history[i] > max) ? history[i] : max;
+        min = (history[i] < min) ? history[i] : min;
     }
 
     double range = max - min;
-
     double scale = range / graph_height;
     double offset = min;
     double center = (max + min) / 2;
+
     // Print colored max, min, range, scale, offset, center, keys should be blue and values should be green in the format "key: value"
     printf(BLUE "max: " GREEN "%.0lf  " BLUE "min: " GREEN "%.0lf  " BLUE "range: " GREEN "%.0lf  " BLUE "scale: " GREEN "%.0lf  " BLUE "offset: " GREEN "%.0lf  " BLUE "center: " GREEN "%.0lf\n" RESET,
            max, min, range, scale, offset, center);
-    printf("           +");
-    for (int i = 0; i < size; i++) {
-        printf("-");
-    }
-    printf("\n");
 
-    /**
-     * Print the graph centered around the average value in history, use graph_width and graph_height
-     * to determine the size of the graph also have values on the left and right side of the graph
-     * to show the range of values in history. The graph should be drawn using the '*' character.
-     * do not fill in the spaces between the axis and the "*" characters.
-     */
+    // Print the top and bottom borders of the graph
+    char border[size + 1];
+    memset(border, '-', size);
+    border[size] = '\0';
+
+    printf("           +%s\n", border);
+    // This loop iterates graph_height times, once for each line of the graph
     for (int i = 0; i < graph_height; i++) {
-        // Print the left side of the graph colored
-        printf(GREEN "%10.0lf "RESET"|", center + ((double)graph_height / 2 - i) * scale);
+        // Print the y-axis value for this line of the graph. The value is calculated as the center value plus or minus an offset, depending on the iteration number. The offset is calculated by multiplying the scale value by the difference between the half the height of the graph and the current iteration number. The scale value determines the scaling of the y-axis, with each character on the y-axis representing a certain number of units.
+        printf(GREEN "%10.0lf "RESET"|", center + ((double) graph_height / 2 - i) * scale);
+
+        // This loop iterates over the values in the history array
         for (int j = 0; j < size; j++) {
-            if (history[j] > center) {
-                if (history[j] >= (max - (i * scale)) && history[j] < (max - ((i - 1) * scale))) {
-                    printf(BLUE "*" RESET);
-                } else {
-                    printf(" ");
-                }
+            // Determine the range represented by the current y-axis value
+            double range_min = (history[j] > center) ? max - (i * scale) : max - ((i + 1) * scale);
+            double range_max = (history[j] > center) ? max - ((i - 1) * scale) : max - (i * scale);
+
+            // If the value falls within the range, print a blue asterisk character. If it doesn't, print a space character.
+            if (history[j] >= range_min && history[j] < range_max) {
+                printf(BLUE "*" RESET);
             } else {
-                if (history[j] <= (max - (i * scale)) && history[j] >= (max - ((i + 1) * scale))) {
-                    printf(BLUE "*" RESET);
-                } else {
-                    printf(" ");
-                }
+                printf(" ");
             }
         }
+        // After the inner loop completes, print a newline character to move to the next line on the graph.
         printf("\n" RESET);
     }
 
-    printf("           +");
-    for (int i = 0; i < size; i++) {
-        printf("-");
-    }
-    printf("\n");
+    printf("           +%s\n", border);
 }
 
 // Function to parse memory info from /proc/meminfo zswap, zswapped, compression, active, inactive, free
@@ -131,20 +122,22 @@ void parse_memory_info(double *zswap, double *zswapped, double *compression, dou
                        double *free) {
     // Parse the values from /proc/meminfo
     FILE *file = fopen("/proc/meminfo", "r");
-    if (file != NULL) {
-        char line[256];
-        while (fgets(line, sizeof(line), file) != NULL) {
-            sscanf(line, "Zswap: %lf", zswap);
-            sscanf(line, "Zswapped: %lf", zswapped);
-            sscanf(line, "Active(anon): %lf", active);
-            sscanf(line, "Inactive(anon): %lf", inactive);
-            sscanf(line, "MemFree: %lf", free);
-        }
-        fclose(file);
-
-        // Calculate the compression value
-        *compression = *zswapped / *zswap;
+    if (file == NULL) {
+        return;
     }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        sscanf(line, "Zswap: %lf", zswap);
+        sscanf(line, "Zswapped: %lf", zswapped);
+        sscanf(line, "Active(anon): %lf", active);
+        sscanf(line, "Inactive(anon): %lf", inactive);
+        sscanf(line, "MemFree: %lf", free);
+    }
+    fclose(file);
+
+    // Calculate the compression value
+    *compression = *zswapped / *zswap;
 }
 
 // Signal handler to set the keep_running flag to 0 when Ctrl+C is pressed
